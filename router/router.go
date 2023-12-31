6d80f532cf29e6fda8cc72325b94f24ab6fd5e85
hanzug <1373338025@qq.com>
2023-12-31 15:53:27 +0800

    docker

--- router/router.go
+++ router/router.go
@@ -43,7 +43,7 @@ func SetupRouter(mode string) *gin.Engine {
 	v1.POST("/login", controller.LoginHandler)
 
 	// 根据时间或分数获取帖子列表
-	v1.GET("/posts2", controller.GetPostListHandler)
+	v1.GET("/posts2", controller.GetPostListHandler2)
 	v1.GET("/posts", controller.GetPostListHandler)
 	v1.GET("/community", controller.CommunityHandler)
 	v1.GET("/community/:id", controller.CommunityDetailHandler)
